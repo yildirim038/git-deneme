6c7c0a615a79f341ddc198fb0b50d4ae5f185dcf
Ali Osman YILDIRIM <aosmanyildirim@gmail.com>
2020-12-03 21:26:51 +0100

    main branchde  ilk uc satir degisti

--- conflict.js
+++ conflict.js
@@ -1,1 +1,3 @@
-// Bu aciklama main branchde eklendi. 
+// Bu aciklama main branchde eklendi. mainde iken tekrar degisti
+let  main = "main branchde eklendi";
+let farkliSatir = "mainde farkli satira bir degisken daha eklendi";     
